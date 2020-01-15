eb429846d76eb40f5a3653e02ddd9cab6cf8b779
Will Dietz <w@wdtz.org>
2020-01-14 22:53:49 -0600

    debug

--- src/spectraluser.cpp
+++ src/spectraluser.cpp
@@ -7,7 +7,6 @@ QColor SpectralUser::color() {
   double pr, pg, pb;
   hsluv::hsluv2rgb(hueF() * 360.0, 100.0, 30.0, &pr, &pg, &pb);
 
-  printf("%g %g %g\n", pr, pg, pb);
-  printf("%g\n", hueF());
-  return QColor::fromRgbF(pr, pg, pb);
+  printf("r: %g g: %g b: %g, hueF: %g, hueF * 360.0: %g\n", pr, pg, pb, hueF(), hueF() * 360.0);
+  return QColor::fromRgbF(qreal(pr), qreal(pg), qreal(pb));
 }
