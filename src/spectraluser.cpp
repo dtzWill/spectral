3b6ff1b15836fc90b3efa0448a38d18130e55eb3
Will Dietz <w@wdtz.org>
2020-01-14 23:16:35 -0600

    saturation 99 instead

--- src/spectraluser.cpp
+++ src/spectraluser.cpp
@@ -1,12 +1,9 @@
 #include "spectraluser.h"
 #include "hsluv.h"
 
-#include <stdio.h>
-
 QColor SpectralUser::color() {
   double pr, pg, pb;
-  hsluv::hsluv2rgb(hueF() * 360.0, 100.0, 30.0, &pr, &pg, &pb);
+  hsluv::hsluv2rgb(hueF() * 360, 99., 30., &pr, &pg, &pb);
 
-  printf("r: %g g: %g b: %g, hueF: %g, hueF * 360.0: %g\n", pr, pg, pb, hueF(), hueF() * 360.0);
-  return QColor::fromRgbF(qreal(pr), qreal(pg), qreal(pb));
+  return QColor::fromRgbF(pr, pg, pb);
 }
