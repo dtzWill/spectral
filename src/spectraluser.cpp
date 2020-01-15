6b6885a241e92e39cc65df0534648f42c25620c4
Will Dietz <w@wdtz.org>
2020-01-14 22:15:57 -0600

    print color, let's see...

--- src/spectraluser.cpp
+++ src/spectraluser.cpp
@@ -1,8 +1,12 @@
 #include "spectraluser.h"
 #include "hsluv.h"
 
+#include <stdio.h>
+
 QColor SpectralUser::color() {
   double pr, pg, pb;
   hsluv2rgb(hueF() * 360.0, 100.0, 30.0, &pr, &pg, &pb);
+
+  printf("%g %g %g\n", pr, pg, pb);
   return QColor::fromRgbF(pr, pg, pb);
 }
